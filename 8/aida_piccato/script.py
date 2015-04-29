a93699a75a96590bde1839b8d4deb84dd8283b66
apiccato <aidapiccato@gmail.com>
2015-04-29 14:02:53 -0400

    Added base files

---
+++ 8/aida_piccato/script.py
@@ -0,0 +1,49 @@
+import mdl
+from display import *
+from matrix import *
+from draw import *
+
+def run(filename):
+    """
+    This function runs an mdl script
+    """
+    color = [255, 255, 255]
+    tmp = new_matrix()
+    ident( tmp )
+
+    p = mdl.parseFile(filename)
+
+    if p:
+        (commands, symbols) = p
+    else:
+        print "Parsing failed."
+        return
+
+    stack = [ tmp ]
+    screen = new_screen()
+        
+    for command in commands:
+        print command
+
+def push():
+    pass
+
+def pop():
+
+def move():
+
+def rotate():
+
+def scale():
+
+def box():
+
+def sphere():
+
+def torus():
+
+def line():
+
+def save():
+
+def display():
